4e5db2428694cd4706236e63f6f96fdb5cc17638
Maurício Caminha <mauricio.caminha@hotmail.com>
2024-12-23 11:28:19 -0300

    adiciona migrations

--- db/migrations/20241221173030_create-users.ts
+++ db/migrations/20241221173030_create-users.ts
@@ -4,8 +4,14 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("users", (table) => {
     table.uuid("id").primary();
     table.string("email").notNullable();
+    table.string("nome").notNullable();
     table.string("senha").notNullable();
     table.timestamp("criado_as").defaultTo(knex.fn.now()).notNullable();
+    table.integer("total_refeicoes").notNullable().defaultTo(0);
+    table.integer("qtd_refeicoes_dentro_dieta").notNullable().defaultTo(0);
+    table.integer("qtd_refeicoes_fora_dieta").notNullable().defaultTo(0);
+    table.integer("sequencia_refeicoes").notNullable().defaultTo(0);
+    table.timestamp("atualizado_as").defaultTo(knex.fn.now()).notNullable();
   });
 }
 
