14ca068c6c033f8109aec289380714cd2695f555
Maurício Caminha <mauricio.caminha@hotmail.com>
2024-12-23 16:48:34 -0300

    adiciona teste `POST /users` e `GET /users/user`

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -1,5 +1,12 @@
 import { execSync } from "node:child_process";
-import { afterAll, beforeAll, beforeEach, describe, test } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+} from "vitest";
 import request from "supertest";
 
 import { app } from "../src/app";
@@ -23,8 +30,32 @@ describe("Users routes", () => {
       .post("/users")
       .send({
         email: "example@email.com",
+        nome: "Example",
         senha: "example",
       })
       .expect(201);
+  });
+
+  test("Should be able to get user metrics", async () => {
+    const createUserResponse = await request(app.server).post("/users").send({
+      email: "example@email.com",
+      nome: "Example",
+      senha: "example",
+    });
+
+    const cookies = createUserResponse.get("Set-Cookie") ?? [];
+
+    const getUserMetricsResponse = await request(app.server)
+      .get("/users/user")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(getUserMetricsResponse.body.user).toEqual({
+      nome: "Example",
+      total_refeicoes: 0,
+      qtd_refeicoes_dentro_dieta: 0,
+      qtd_refeicoes_fora_dieta: 0,
+      sequencia_refeicoes: 0,
+    });
   });
 });
