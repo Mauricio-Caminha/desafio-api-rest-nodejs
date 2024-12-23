cfc349f04c9bb490ace46ec4a86d36577462ef6b
Maurício Caminha <mauricio.caminha@hotmail.com>
2024-12-23 16:44:19 -0300

    adiciona rotas `POST /users` e `GET /users/user` e middleware

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,15 +3,17 @@ import { randomUUID } from "node:crypto";
 import { z } from "zod";
 
 import { knex } from "../database";
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/", async (request, reply) => {
     const userSchema = z.object({
       email: z.string().email(),
+      nome: z.string().min(3),
       senha: z.string().min(3),
     });
 
-    const { email, senha } = userSchema.parse(request.body);
+    const { email, nome, senha } = userSchema.parse(request.body);
 
     let session_id = request.cookies.session_id;
     if (!session_id) {
@@ -25,10 +27,39 @@ export async function usersRoutes(app: FastifyInstance) {
     await knex("users").insert({
       id: randomUUID(),
       email,
+      nome,
       senha,
       session_id,
     });
 
     reply.status(201).send();
   });
+
+  app.get(
+    "/user",
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const session_id = request.cookies.session_id;
+
+      const user = await knex("users")
+        .select(
+          "nome",
+          "total_refeicoes",
+          "qtd_refeicoes_dentro_dieta",
+          "qtd_refeicoes_fora_dieta",
+          "sequencia_refeicoes"
+        )
+        .where({ session_id })
+        .first();
+
+      if (!user) {
+        reply.status(404).send();
+        return;
+      }
+
+      reply.send({ user });
+    }
+  );
 }
